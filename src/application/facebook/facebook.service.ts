6c10bb146e05fb60fb42c16abca474646949efaf
thobui14ig <buithanhtho14ig@gmail.com>
2025-08-26 22:12:28 +0700

    ok

---
+++ src/application/facebook/facebook.service.ts
@@ -0,0 +1,94 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import * as dayjs from 'dayjs';
+import * as utc from 'dayjs/plugin/utc';
+import { firstValueFrom } from 'rxjs';
+import { isNumeric } from 'src/common/utils/check-utils';
+import { DataSource } from 'typeorm';
+import { CommentsService } from '../comments/comments.service';
+import { LinkEntity } from '../links/entities/links.entity';
+import { ProxyEntity } from '../proxy/entities/proxy.entity';
+import { FB_UUID } from './facebook.service.i';
+import { GetCommentPrivateUseCase } from './usecase/get-comment-private/get-comment-private';
+import { GetCommentPublicUseCase } from './usecase/get-comment-public/get-comment-public';
+
+dayjs.extend(utc);
+// dayjs.extend(timezone);
+
+@Injectable()
+export class FacebookService {
+  // appId = '256002347743983';
+  appId = '6628568379'
+  fbUrl = 'https://www.facebook.com';
+  fbGraphql = `https://www.facebook.com/api/graphql`;
+  ukTimezone = 'Asia/Bangkok';
+  browser = null
+
+  constructor(private readonly httpService: HttpService,
+    private getCommentPublicUseCase: GetCommentPublicUseCase,
+    private getCommentPrivateUseCase: GetCommentPrivateUseCase,
+    private commentsService: CommentsService,
+    private connection: DataSource
+  ) {
+  }
+
+  async getCmtPublic(postIdStr: string, link: LinkEntity) {
+    const commentsRes = await this.getCommentPublicUseCase.getCmtPublic(postIdStr, false, link)
+    if (!commentsRes) {//hết proxy or token
+      return null
+    }
+
+    return commentsRes.data
+  }
+
+  async getCommentByToken(postId: string, postIdV1?: string) {
+    const commentsRes = await this.getCommentPrivateUseCase.getCommentPrivate(postId, postIdV1)
+    if (!commentsRes) {//hết proxy or token
+      return null
+    }
+
+    return commentsRes.data
+  }
+
+  async getPhoneNumber(uid: string, commentId: string, accountFbUuid: string) {
+    if (!isNumeric(uid)) return null
+    const dataPhoneDb = await this.commentsService.getPhoneNumber(uid)
+    if (dataPhoneDb?.phoneNumber) return dataPhoneDb.phoneNumber
+    const account = FB_UUID.find(item => item.mail === accountFbUuid)
+    if (!account) return null
+    const body = {
+      key: account.key,
+      uids: [String(uid)]
+    }
+    const response = await firstValueFrom(
+      this.httpService.post("https://api.fbuid.com/keys/convert", body,),
+    );
+    const dataPhone = response.data?.find(item => item.uid == uid)
+    const logs = {
+      body,
+      response: response.data
+    }
+    await this.insertLogs(uid, commentId, JSON.stringify(logs))
+
+    return dataPhone?.phone ?? null
+  }
+
+  async addPhone(UID: string, Phone: string) {
+    try {
+      const body = {
+        UID,
+        Phone
+      }
+      return await firstValueFrom(
+        this.httpService.post("https://api.fbuid.com/conversions/import", body,),
+      );
+    } catch (error) { }
+  }
+
+  insertLogs(UID: string, commentId: string, params: string) {
+    return this.connection.query(`
+      INSERT INTO logs (uid, cmt_id, params)
+      VALUES ('${UID}', '${commentId}', '${params}');  
+    `)
+  }
+}
---
+++ src/application/monitoring/monitoring.service.ts
@@ -0,0 +1,193 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as dayjs from 'dayjs';
+import * as utc from 'dayjs/plugin/utc';
+import { delay, groupPostsByType } from 'src/common/utils/helper';
+import { FacebookService } from '../facebook/facebook.service';
+import { ICommentResponse } from '../facebook/facebook.service.i';
+import {
+  LinkEntity,
+  LinkType
+} from '../links/entities/links.entity';
+import { LinkService } from '../links/links.service';
+import { MonitoringConsumer } from './monitoring.process';
+import { Cron, CronExpression } from '@nestjs/schedule';
+const proxy_check = require('proxy-check');
+
+dayjs.extend(utc);
+
+type RefreshKey = 'refreshToken' | 'refreshCookie' | 'refreshProxy';
+@Injectable()
+export class MonitoringService {
+  linkIdsReceive: number[] = []
+  postIdRunning: string[] = []
+  linksPublic: LinkEntity[] = []
+  linksPrivate: LinkEntity[] = []
+  isHandleUrl: boolean = false
+  isReHandleUrl: boolean = false
+  isHandleUuid: boolean = false
+  isCheckProxy: boolean = false
+  isUpdatePostIdV1: boolean = false
+  linkPublicCheckSpeed: LinkEntity | null = null
+  speed = 0
+
+  constructor(
+    private readonly facebookService: FacebookService,
+    private linkService: LinkService,
+    private consumer: MonitoringConsumer
+  ) { }
+
+  @Cron(CronExpression.EVERY_10_SECONDS)
+  async startMonitoring() {
+    const postsStarted = await this.linkService.getPostStarted()
+    this.linkPublicCheckSpeed = postsStarted.find(item => item.postIdV1)
+    const groupPost = groupPostsByType(postsStarted || []);
+    for (const element of postsStarted) {
+      const itemPublic = this.linksPublic.find(item => item.id === element.id)
+      if (itemPublic) {
+        itemPublic.delayTime = element.delayTime
+      }
+
+      const itemPrivate = this.linksPrivate.find(item => item.id === element.id)
+      if (itemPrivate) {
+        itemPrivate.delayTime = element.delayTime
+      }
+    }
+
+    return Promise.all([this.handleStartMonitoring((groupPost.public || []), LinkType.PUBLIC), this.handleStartMonitoring((groupPost.private || []), LinkType.PRIVATE)])
+  }
+
+  handleStartMonitoring(links: LinkEntity[], type: LinkType) {
+    let oldLinksRunning = []
+    if (type === LinkType.PUBLIC) {
+      oldLinksRunning = this.linksPublic
+    } else {
+      oldLinksRunning = this.linksPrivate
+    }
+
+
+    const oldIdsSet = new Set(oldLinksRunning.map(item => item.id));
+    const linksRunning = links.filter(item => !oldIdsSet.has(item.id));
+
+    if (type === LinkType.PUBLIC) {
+      this.linksPublic = links
+      return this.handlePostsPublic(linksRunning)
+    }
+    else {
+      this.linksPrivate = links
+      return this.handlePostsPrivate(linksRunning)
+    }
+  }
+
+  async processLinkPublic1(link: LinkEntity) {
+    if (!link.postIdV1) {
+      const runThread = async (threadOrder: number) => {
+        while (true) {
+          const linkRuning = this.linksPublic.find(item => item.id === link.id)// check còn nằm trong link
+          if (!linkRuning) { break };
+          if (threadOrder > linkRuning.thread) { break };
+
+          try {
+            let dataComment = await this.facebookService.getCmtPublic(link.postId, link)
+            if (!dataComment?.commentId || !dataComment?.userIdComment) continue
+            this.addQueueComment(dataComment, link)
+
+          } catch (error) {
+            console.log(`Crawl comment with postId ${link.postId} Error.`, error?.message)
+          } finally {
+            if (link.delayTime) {
+              await delay((linkRuning.delayTime) * 1000)
+            }
+          }
+        }
+      }
+
+      for (let threadOrder = 1; threadOrder <= link.thread; threadOrder++) {
+        runThread(threadOrder);
+      }
+    }
+  }
+
+  async processLinkPublic2(link: LinkEntity) {
+    if (link.postIdV1) {
+      const runThread = async (threadOrder: number) => {
+        while (true) {
+          const start = Date.now();
+          const linkRuning = this.linksPublic.find(item => item.id === link.id)
+          if (!linkRuning) { break };
+          if (threadOrder > linkRuning.thread) { break };
+
+          try {
+            let dataComment = await this.facebookService.getCmtPublic(link.postIdV1, link) || {} as any
+            if (!dataComment?.commentId || !dataComment?.userIdComment) continue
+            this.addQueueComment(dataComment, link)
+
+          } catch (error) {
+            console.log(`Crawl comment with postId ${link.postId} Error.`, error?.message)
+          } finally {
+            const end = Date.now();
+            const duration = (end - start) / 1000;
+            // console.log(duration)
+            this.speed = duration
+            if (link.delayTime) {
+              await delay((linkRuning.delayTime) * 1000)
+            }
+          }
+
+        }
+      }
+      for (let threadOrder = 1; threadOrder <= link.thread; threadOrder++) {
+        runThread(threadOrder);
+      }
+    }
+  }
+
+  async handlePostsPublic(linksRunning: LinkEntity[]) {
+    const postHandle = linksRunning.map((link) => {
+      return this.processLinkPublic1(link)
+    })
+    const postHandleV1 = linksRunning.map((link) => {
+      return this.processLinkPublic2(link)
+    })
+
+    return Promise.all([...postHandle, ...postHandleV1])
+  }
+
+  async processLinkPrivate(link: LinkEntity) {
+    const runThread = async (threadOrder: number) => {
+      while (true) {
+        const linkRuning = this.linksPrivate.find(item => item.id === link.id)
+        if (!linkRuning) { break };
+        if (threadOrder > linkRuning.thread) { break };
+
+        try {
+          const dataComment = await this.facebookService.getCommentByToken(link.postId, link.postIdV1)
+          if (!dataComment?.commentId || !dataComment?.userIdComment) continue
+          this.addQueueComment(dataComment, link)
+        } catch (error) {
+          console.log(`Crawl comment with postId ${link.postId} Error.`, error?.message)
+        } finally {
+          if (link.delayTime) {
+            await delay((linkRuning.delayTime) * 1000)
+          }
+        }
+      }
+    }
+    for (let threadOrder = 1; threadOrder <= link.thread; threadOrder++) {
+      runThread(threadOrder);
+    }
+
+  }
+
+  async handlePostsPrivate(linksRunning: LinkEntity[]) {
+    const postHandle = linksRunning.map((link) => {
+      return this.processLinkPrivate(link)
+    })
+
+    return Promise.all(postHandle)
+  }
+
+  async addQueueComment(resComment: ICommentResponse, link: LinkEntity) {
+    return this.consumer.run(link, resComment)
+  }
+}
